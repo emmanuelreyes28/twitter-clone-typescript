02ca33dff953495b5dcdfb6d8b93f954433a6024
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-07-13 23:31:16 -0700

    add post request functionality to signin form

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -9,11 +11,59 @@ import {
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+type Login = {
+  username: string;
+  password: string;
+};
 
 export default function SignIn() {
+  const [data, setData] = useState<Login>({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState<string>("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setData((prevValue): Login => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("api/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const { message } = await response.json();
+        setError(message);
+        console.log(message);
+      } else {
+        //TO-DO: route to timeline if signin successful
+        console.log("User successfully loggedin");
+      }
+    } catch (error) {
+      console.error("An error occurred: ", error);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
-      <form className="max-w-xl w-full px-4">
+      <form onSubmit={handleSubmit} className="max-w-xl w-full px-4">
         <Card>
           <CardHeader className="space-y-1">
             <CardTitle className="text-2xl">Blue Bird</CardTitle>
@@ -22,11 +72,25 @@ export default function SignIn() {
           <CardContent className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="username">Username</Label>
-              <Input id="username" type="text" name="username" />
+              <Input
+                id="username"
+                type="text"
+                name="username"
+                value={data.username}
+                onChange={handleChange}
+                required
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" name="password" />
+              <Input
+                id="password"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                required
+              />
             </div>
           </CardContent>
           <CardFooter>
@@ -34,6 +98,11 @@ export default function SignIn() {
               Sign-in
             </Button>
           </CardFooter>
+          {error && (
+            <CardDescription className="text-center text-red-600 pb-2">
+              {error}
+            </CardDescription>
+          )}
         </Card>
       </form>
     </div>
