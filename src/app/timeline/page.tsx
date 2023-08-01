90c54dc8f969df8b22d5a27e393caeffbc8e1283
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-07-31 21:43:04 -0700

    getProfile username for compose-tweet component

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,14 +1,23 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { Database } from "@/types/supabase";
 import ComposeTweet from "@/components/compose-tweet";
 import LogoutButton from "@/components/logout-button";
 
-export default function Timeline() {
+export default async function Timeline() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <div>
       <div className="grid justify-items-end">
         <LogoutButton />
       </div>
       <div className="flex items-center justify-center">
-        <ComposeTweet></ComposeTweet>
+        <ComposeTweet session={session} />
       </div>
     </div>
   );
--- src/components/compose-tweet.tsx
+++ src/components/compose-tweet.tsx
@@ -1,13 +1,54 @@
+"use client";
+
 import { Card } from "./ui/card";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
+import {
+  createClientComponentClient,
+  Session,
+} from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/types/supabase";
+import { useState, useEffect, useCallback } from "react";
 
-export default function ComposeTweet() {
+export default function ComposeTweet({ session }: { session: Session | null }) {
+  const supabase = createClientComponentClient<Database>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [username, setUsername] = useState<string | null>(null);
+  const user = session?.user;
+
+  const getProfile = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      let { data, error, status } = await supabase
+        .from("profiles")
+        .select(`username`)
+        .eq("id", user?.id)
+        .single();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+      }
+    } catch (error) {
+      alert("Error loading user data!");
+    } finally {
+      setLoading(false);
+    }
+  }, [user, supabase]);
+
+  useEffect(() => {
+    getProfile();
+  }, [user, getProfile]);
+
   return (
     <div className="pt-5 mt-3 border-black border rounded-md w-1/2">
       <div className="pb-3 pl-4">
-        <Label>@username</Label>
+        <Label>@{username}</Label>
       </div>
       <div className="px-4">
         <Card>
